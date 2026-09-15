d8a1ac53fde20772f763d91054407a8895ccbad5
Kovalenko Vitaliy <vk280694@gmail.com>
2026-09-15 18:38:45 +0300

    Create 02_greet.py

---
+++ Tasks/02_greet.py
@@ -0,0 +1,9 @@
+def greet (name:str=""):
+    if name!="":
+        return f"Привет, {name}!"
+    else:
+        return f"Привет!"
+if __name__ == "__main__":
+    assert greet("Виталий") == "Привет, Виталий!"
+    assert greet("Анна") == "Привет, Анна!"
+    assert greet("") == "Привет!"
