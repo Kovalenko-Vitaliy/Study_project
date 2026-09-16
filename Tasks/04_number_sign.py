e8d382f4bf80f3ae0b1c411a7f077b4a78c47efc
Kovalenko Vitaliy <vk280694@gmail.com>
2026-09-16 10:53:00 +0300

    Create 04_number_sign.py

---
+++ Tasks/04_number_sign.py
@@ -0,0 +1,16 @@
+def number_sign(number):
+    if number>0:
+        return "positive"
+    if number<0:
+        return "negative"
+    else:
+        return "zero"
+    
+
+
+if __name__ == "__main__":
+    assert number_sign(5) == "positive"
+    assert number_sign(-8) == "negative"
+    assert number_sign(0) == "zero"
+
+#Работает :)
