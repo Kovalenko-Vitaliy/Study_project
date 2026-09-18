05559061c992114dadf1d16470c6c95e51c15970
Kovalenko Vitaliy <vk280694@gmail.com>
2026-09-18 12:04:20 +0300

    Create 09_first_or_none.py

---
+++ exercises/09_first_or_none.py
@@ -0,0 +1,14 @@
+def first_or_none (numbers:list[int])->int|None:
+    if numbers:
+        return numbers[0]
+    return None
+
+if __name__ == "__main__":
+    assert first_or_none([10, 20, 30]) == 10
+    assert first_or_none([0, 5]) == 0
+    assert first_or_none([-7]) == -7
+    assert first_or_none([]) is None
+
+    assert first_or_none([0]) ==0
+
+#Работает :)
