8142f03323669ad882ee02a3a4c021687f95dad4
Kovalenko Vitaliy <vk280694@gmail.com>
2026-09-19 12:37:14 +0300

    Update 10_sum_numbers.py

--- exercises/10_sum_numbers.py
+++ exercises/10_sum_numbers.py
@@ -10,4 +10,9 @@ if __name__ == "__main__":
 
     assert sum_numbers([10,-10,5]) == 5
 
+    numbers = [2, -1, 4]
+    before = numbers.copy()
+    assert sum_numbers(numbers) == 5
+    assert numbers == before
+
 #Работает :)
