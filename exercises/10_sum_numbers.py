e1669852a590b5dfa8037316874a0677ba7c79a1
Kovalenko Vitaliy <vk280694@gmail.com>
2026-09-19 11:49:30 +0300

    Create 10_sum_numbers.py

---
+++ exercises/10_sum_numbers.py
@@ -0,0 +1,13 @@
+def sum_numbers(numbers: list) -> int:
+    return sum(numbers)
+
+if __name__ == "__main__":
+    assert sum_numbers([1, 2, 3]) == 6
+    assert sum_numbers([-2, 5, -3]) == 0
+    assert sum_numbers([-3, -4]) == -7
+    assert sum_numbers([7]) == 7
+    assert sum_numbers([]) == 0
+
+    assert sum_numbers([10,-10,5]) == 5
+
+#Работает :)
