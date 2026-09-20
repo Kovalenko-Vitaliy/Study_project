1950c2c0fd6a6d127504e870dc80426285a5e432
Kovalenko Vitaliy <vk280694@gmail.com>
2026-09-20 15:58:10 +0300

    Create 12_square_numbers.py

---
+++ exercises/12_square_numbers.py
@@ -0,0 +1,15 @@
+def square_numbers(numbers: list) -> list:
+    return [number ** 2 for number in numbers]
+
+if __name__ == "__main__":
+    assert square_numbers([1, 2, 3]) == [1, 4, 9]
+    assert square_numbers([-3, 0, 2]) == [9, 0, 4]
+    assert square_numbers([2, 2]) == [4, 4]
+    assert square_numbers([]) == []
+
+    original=[0,4,-7,15,3]
+    copy_before=original.copy()
+    assert square_numbers(original) == [0,16,49,225,9]
+    assert original == copy_before
+
+#Работает :)
